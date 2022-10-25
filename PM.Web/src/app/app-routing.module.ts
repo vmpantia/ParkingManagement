e97c08de02fb281f84a679436f19d950e2157216
Vincent Pantia <vincent.m.pantia@hotmail.com>
2022-10-25 19:35:00 +0800

    [u] Customer UI Changes

--- PM.Web/src/app/app-routing.module.ts
+++ PM.Web/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { CustomerComponent } from './customer/customer.component';
+import { AddEditCustomerComponent } from './customer/add-edit-customer/add-edit-customer.component';
 
 const routes: Routes = [
-  {path: 'customer', component:CustomerComponent}
+  {path: 'customer', component:CustomerComponent},
+  {path: 'customer/add-edit-customer', component:AddEditCustomerComponent}
 ];
 
 @NgModule({
